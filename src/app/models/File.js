5addc9c3c2859190b6a9cab0e1d1fd7729e80954
Nathan Rios <nathan.cdc@gmail.com>
2019-11-03 22:28:55 -0300

    add providers index

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -7,6 +7,12 @@ class File extends Model {
         // campos que iremos receber dos usuários
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize, // argumento da init()
--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,12 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
-const upload = new multer(multerConfig);
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -18,6 +19,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware); // só as rotas abaixo dessa global é autenticada
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
